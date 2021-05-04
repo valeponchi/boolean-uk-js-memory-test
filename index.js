e67757d3eaf8d589511222511485a8c5f25e49b7
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-04 22:59:04 +0100

    done last part of the assignment (no challenges)

--- index.js
+++ index.js
@@ -12,40 +12,33 @@ for (let i=1; i<=4; i++ ) {
   randomNumbers.push(number)
 }
 
-alert(`Memorize these random numbers:  ${randomNumbers}`) //user sees the numbers
-
-
-//repeat this 4 times
 //show to user the 4 random numbers
-
-// const randomNumbers = [23, 45, 76, 89]  //creo array coi random numeri
-
+alert(`How many of these numbers will you remember after you press "Ok"?:  ${randomNumbers}`) //user sees the numbers
 
 
 // - After 15 seconds, 
 // ask them to enter each number in order
-let userTypedNumbers = []
+let userTypedNumbers
 function askTheNumbers() {
   let usersNumbers = prompt(`Can you write the numbers in order?`)
-  usersNumbers = usersNumbers.split(" ") //gives me an array with inside the array with the numbers typed
-  return userTypedNumbers.push(usersNumbers)
+  userTypedNumbers = usersNumbers.split(" ").map(Number) 
+
+  console.log(`These are the user typed numbers: `, userTypedNumbers) 
+  checkMatches() //function in full follows this one. We need to use it here, inside this one. so it runs at the right moment in time. 
 }  
-setTimeout(askTheNumbers, 2000)
-console.log(`These are the user typed numbers: `, userTypedNumbers) //the array with the User typed numbers
-
-
 
 // - Let them know how many they got right!
+function checkMatches() {
+  let counter = 0;
+  for (let i = 0; i < randomNumbers.length; i++) {
+    const number = randomNumbers[i];     //use i also to point to a specific element into the array
+    const guess = userTypedNumbers[i];   // same here
+    if (guess === number) {              //if the 2 element in the same position inside the 2 arrays, match, then..
+      counter += 1;                      // then +1 al counter 
+    }
+  }
+  alert(`You gave ${counter} correct answers.`)
+}
 
+setTimeout(askTheNumbers, 4000)
 
-// DOESN'T WORK :
-// let correctNumbers = userTypedNumbers.filter(function (number){return number === randomNumbers})
-// setTimeout(alert(correctNumbers), 2000)
-
-
-
-
-
-// DOMANDE: 
-// perche mi da un array con dentro un array con i numeri dell User 
-// l ultimo alert avviene prima che l user metta i numeri e non funziona nemmeno
